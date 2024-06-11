ee3452e4cbedac9e62d352afd8da8ca7e0875a13
liuxiaobleach <1241368737@qq.com>
2024-06-11 18:26:03 +0800

    update

--- keccak-air/examples/prove_goldilocks_poseidon_fibonacci.rs
+++ keccak-air/examples/prove_goldilocks_poseidon_fibonacci.rs
@@ -1,25 +1,37 @@
+use ark_ff::{BigInteger, PrimeField};
+use rand::thread_rng;
 use p3_challenger::DuplexChallenger;
 use p3_commit::ExtensionMmcs;
 use p3_dft::Radix2DitParallel;
 use p3_field::extension::BinomialExtensionField;
-use p3_field::Field;
+use p3_field::{AbstractField, Field};
 use p3_fri::{FriConfig, TwoAdicFriPcs};
-use p3_goldilocks::{Goldilocks, MdsMatrixGoldilocks};
-use p3_keccak_air::{FibonacciAir, generate_trace_rows};
+use p3_goldilocks::{DiffusionMatrixGoldilocks, Goldilocks, MdsMatrixGoldilocks};
+use p3_keccak_air::{FibonacciAir, NUM_FIBONACCI_COLS};
+use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
 use p3_merkle_tree::FieldMerkleTreeMmcs;
-use p3_poseidon::Poseidon;
-use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
-use p3_uni_stark::{prove, verify, StarkConfig, VerificationError};
+use p3_symmetric::{PaddingFreeSponge, Permutation, TruncatedPermutation};
+use p3_uni_stark::{get_log_quotient_degree, prove, verify, StarkConfig, VerificationError};
 use p3_util::log2_ceil_usize;
-use rand::{random, thread_rng};
 use tracing_forest::util::LevelFilter;
 use tracing_forest::ForestLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry};
+use zkhash::fields::goldilocks::FpGoldiLocks;
+use zkhash::poseidon::poseidon_instance_goldilocks::RC12;
+use p3_poseidon::Poseidon;
 
-const NUM_HASHES: usize = 1;
+const WIDTH: usize = 12;
+
+fn goldilocks_from_ark_ff(input: FpGoldiLocks) -> Goldilocks {
+    let as_bigint = input.into_bigint();
+    let mut as_bytes = as_bigint.to_bytes_le();
+    as_bytes.resize(8, 0);
+    let as_u64 = u64::from_le_bytes(as_bytes[0..8].try_into().unwrap());
+    Goldilocks::from_wrapped_u64(as_u64)
+}
 
 fn main() -> Result<(), VerificationError> {
     let env_filter = EnvFilter::builder()
@@ -60,9 +72,24 @@ fn main() -> Result<(), VerificationError> {
 
     type Challenger = DuplexChallenger<Val, Perm, 8>;
 
-    let inputs = (0..NUM_HASHES).map(|_| random()).collect::<Vec<_>>();
-    let trace = generate_trace_rows::<Val>(inputs);
-
+    const NUM_FIBONACCI_ROWS: usize = 64;
+    let mut values: Vec<Vec<u64>> = Vec::with_capacity(NUM_FIBONACCI_ROWS);
+    values.push(vec![1, 1, 2]);
+    for i in 1..NUM_FIBONACCI_ROWS {
+        values.push(vec![
+            values[i - 1][1],
+            values[i - 1][2],
+            values[i - 1][1] + values[i - 1][2],
+        ]);
+    }
+    let trace = RowMajorMatrix {
+        values: values
+            .into_iter()
+            .flatten()
+            .map(|x| Val::from_canonical_u64(x))
+            .collect::<Vec<_>>(),
+        width: NUM_FIBONACCI_COLS,
+    };
     let fri_config = FriConfig {
         log_blowup: 1,
         num_queries: 100,
@@ -70,17 +97,30 @@ fn main() -> Result<(), VerificationError> {
         mmcs: challenge_mmcs,
     };
     type Pcs = TwoAdicFriPcs<Val, Dft, ValMmcs, ChallengeMmcs>;
+
+    dbg!(log2_ceil_usize(trace.height()));
+    dbg!(get_log_quotient_degree::<Val, FibonacciAir>(
+        &FibonacciAir {},
+        0
+    ));
+
     let pcs = Pcs::new(log2_ceil_usize(trace.height()), dft, val_mmcs, fri_config);
+
 
     type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
     let config = MyConfig::new(pcs);
 
     let mut challenger = Challenger::new(perm.clone());
 
-    let proof = prove(&config, &FibonacciAir {}, &mut challenger, trace, &vec![]);
+    let proof = prove::<MyConfig, _>(&config, &FibonacciAir {}, &mut challenger, trace, &vec![]);
 
-    std::fs::write("proof_fibonacci_in_poseidon.json", serde_json::to_string(&proof).unwrap()).unwrap();
+    std::fs::write(
+        "proof_fibonacci.json",
+        serde_json::to_string(&proof).unwrap(),
+    )
+        .unwrap();
 
     let mut challenger = Challenger::new(perm);
-    verify(&config, &FibonacciAir {}, &mut challenger, &proof, &vec![])
+    verify(&config, &FibonacciAir {}, &mut challenger, &proof, &vec![]).unwrap();
+    Ok(())
 }
